41156dd2849e803e381b0b631994c994c8403433
PhaniAlamuri <131159758+PhaniAlamuri@users.noreply.github.com>
2023-07-14 15:26:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let fibonacci = (num) => {
-  return num < 2 ? num : fibonacci(num - 1) + fibonacci(num - 2);
+function fibonacci(num) {
+// your code here
+	if(num <= 1){
+		return num;
+	}
+	return fibonacci(num-1) + fibonacci(num-2);
 }
 
 module.exports = fibonacci;
